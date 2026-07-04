dfebb4935aa407288e609fd4b0532d11155836fc
colony-sketch <amankumarbansal05@gmail.com>
2026-07-04 07:05:21 +0530

    initial st up

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8081;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Home Page :-)"
+  })
+})
+
+// app.all('*', (req, res) => {
+//   res.status(500).json({
+//     message: "Not built yet:-("
+//   })
+// })
+
+
+app.listen(port, () => {
+  console.log(`Server is up and running on http://localhost:${port}`);
+})
